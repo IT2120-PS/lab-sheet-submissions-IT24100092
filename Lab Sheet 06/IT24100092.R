33659b18d13a99bbb1c80e6ad06ca452e27cd15e
IT24100092 <it24100092@my.sliit.lk>
2025-09-02 20:02:01 +0530

    Add IT24100092.R to Lab Sheet 06

---
+++ Lab Sheet 06/IT24100092.R
@@ -0,0 +1,19 @@
+#Exercise
+#01 
+
+#i
+#Binomial Distribution
+#n=50  , p = 0.85
+
+#ii
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#02
+#i
+# number of calls received in one hour
+
+#ii
+#Poisson Distribution
+
+#iii
+dpois(15, 12)
