781b5b2d926e10977af72d03059dd1b3ab6e8f76
IT24100092 <it24100092@my.sliit.lk>
2025-09-16 18:59:15 +0530

    PS_Lab7_R

---
+++ Lab Sheet 07/IT24100092_PS_Lab7.R
@@ -0,0 +1,11 @@
+getwd()
+setwd("C:\\Users\\user\\OneDrive - Sri Lanka Institute of Information Technology\\new\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24100092_Lab7")
+getwd()
+
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+pexp(2, rate=1/3)
+
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+qnorm(0.95, mean=100, sd=15)
